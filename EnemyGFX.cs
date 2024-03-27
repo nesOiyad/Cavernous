51eb6f523396c82efaa9bf58374d14cb13056350
Umut-Canakoglu <148465244+Umut-Canakoglu@users.noreply.github.com>
2024-03-27 10:47:34 +0300

    Update EnemyGFX.cs

--- EnemyGFX.cs
+++ EnemyGFX.cs
@@ -11,9 +11,9 @@ public class EnemyGFX : MonoBehaviour
     public int flyerDamage = 10;
     void Update(){
         if(aiPath.desiredVelocity.x >= 0.01){
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        } else if (aiPath.desiredVelocity.x <= -0.01f){
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.localScale = new Vector3(-6f, 6f, 0.25f);
+        } else if (aiPath.desiredVelocity.x <= -0.01){
+            transform.localScale = new Vector3(6f, 6f, 0.25f);
         }
     }
     public void FlyerDamage(int damage){
